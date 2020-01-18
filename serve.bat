f3949c0398557b0a8a1c405cc7fbc4148c074f29
bh0 <bryce13h0@gmail.com>
2020-01-18 12:12:29 +0000

    manual website change

--- serve.bat
+++ serve.bat
@@ -18,3 +18,5 @@ if "git config remote.gh-pages.url"=="https://github.com/bh0/portrollio-test.git
 git add index.html styles.css custom.css assets .gitignore serve.bat 
 git commit -m "serve via gh-pages"
 git push origin gh-pages
+echo "Returning to master branch"
+git checkout master
