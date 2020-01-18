495f0449f6e45fe3d5405f5b62f90f8f28caaac2
bh0 <bryce13h0@gmail.com>
2020-01-18 12:06:41 +0000

    no message

--- serve.bat
+++ serve.bat
@@ -1,8 +1,20 @@
-REM Will serve via Github Pages 
-REM may need to create new Github repository with Curl + Github API 
+@echo off
+REM REM Will serve via Github Pages 
+REM REM may need to create new Github repository with Curl + Github API 
+REM git switch -c gh-pages 
+REM git remote add gh-pages https://github.com/bh0/portrollio-test.git
+REM git add index.html styles.css custom.css assets .gitignore serve.bat 
+REM git commit -m "serve via gh-pages"
+REM git push origin gh-pages
+REM REM may need to change branch (back to master) 
+
 git switch -c gh-pages 
-git remote add gh-pages https://github.com/bh0/portrollio-test.git
+if "git config remote.gh-pages.url"=="https://github.com/bh0/portrollio-test.git" (
+    echo "adding remote"
+    git remote add gh-pages https://github.com/bh0/portrollio-test.git
+) else (
+    echo "remote exists, continuing"
+)
 git add index.html styles.css custom.css assets .gitignore serve.bat 
 git commit -m "serve via gh-pages"
 git push origin gh-pages
-REM may need to change branch (back to master) 
