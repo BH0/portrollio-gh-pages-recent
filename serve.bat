fb9f6b5a2adf3eac7b084f2719a2c597fd5d513d
bh0 <bryce13h0@gmail.com>
2020-01-18 13:01:29 +0000

    add git pull to serve.bat

--- serve.bat
+++ serve.bat
@@ -9,6 +9,7 @@ REM git push origin gh-pages
 REM REM may need to change branch (back to master) 
 
 git switch -c gh-pages 
+git pull REM not comfortable with this 
 if "git config remote.gh-pages.url"=="https://github.com/bh0/portrollio-test.git" (
     echo "adding remote"
     git remote add gh-pages https://github.com/bh0/portrollio-test.git
